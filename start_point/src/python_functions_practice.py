23072270f5a2e4e1f5ca86b3c36cbba824f669c1
Evan Reid <evan_1995@hotmail.com>
2020-11-04 20:16:59 +0000

    fourth function prob

--- start_point/src/python_functions_practice.py
+++ start_point/src/python_functions_practice.py
@@ -9,3 +9,16 @@ def subtract(num_1, num_2):
     subract_result = num_1 - num_2
     return subract_result
 
+def multiply(num_1, num_2):
+    multiply_result = num_1*num_2
+    return multiply_result
+
+def divide(num_1, num_2):
+      divide_result = num_1 / num_2
+      return divide_result 
+
+def length_of_string(string_length):
+      string = 21
+      return string
+      
+      
