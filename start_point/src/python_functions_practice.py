bf894617b0252ac43bda8fb599efd01046aa1a85
Evan Reid <evan_1995@hotmail.com>
2020-11-04 19:49:06 +0000

    third funct prob

--- start_point/src/python_functions_practice.py
+++ start_point/src/python_functions_practice.py
@@ -4,4 +4,8 @@ def return_10():
 def add(num_1, num_2):
     result = num_1 + num_2
     return result
-    
+
+def subtract(num_1, num_2):
+    subract_result = num_1 - num_2
+    return subract_result
+
